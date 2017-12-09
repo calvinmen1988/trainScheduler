b25673b6cb52f1e5efbfe06fe8028510ee0d4382
Calvin Mendoza <calvinmendoza88@gmail.com>
2017-12-09 11:15:48 -0500

    initial commit

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,20 @@
+//var for tables 
+var trainName = "";
+var destination = "";
+var frequency = "";
+var nextArrival = "";
+var minuteAway = "";
+
+// on click events 
+
+
+// grabbed values from text box 
+
+//firebase scripts 
+
+//function for displaying buttons and adding new buttons
+
+
+
+//on click event for the user form to add more buttons based on topics
+    $("table").on("click", function () {}
